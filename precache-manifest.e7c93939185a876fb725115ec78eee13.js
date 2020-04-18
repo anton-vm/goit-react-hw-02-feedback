9552c47d723e8fc081830e3aec8767800753727b
anton-vm <anton_vm@ukr.net>
2020-04-18 20:16:50 +0300

    Updates

---
+++ precache-manifest.e7c93939185a876fb725115ec78eee13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e83b0ff7c110c4c59d3ed9c53b82dc",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "31e4aa06fd456841c23d",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "42df7efb3f310e7d70ba",
+    "url": "/goit-react-hw-02-feedback/static/js/2.70f8a49d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.70f8a49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e4aa06fd456841c23d",
+    "url": "/goit-react-hw-02-feedback/static/js/main.cda12426.chunk.js"
+  },
+  {
+    "revision": "75552aea3414b74608ba",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.569f9452.js"
+  }
+]);
